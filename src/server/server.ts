abd51f7d89e7cb3ef528c71e872ac692bbd6492e
Shannon Ritenour <shannonritenour@gmail.com>
2021-04-03 10:01:12 -0500

    initial setup

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,22 @@
 import * as express from 'express';
+import * as morgan from 'morgan';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+import * as cors from 'cors';
+import * as path from 'path';
+
 import apiRouter from './routes';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(express.json());
 app.use(apiRouter);
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
