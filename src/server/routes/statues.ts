37dd644422393c6e41983550c7dff51baf3a37d8
Shannon Ritenour <shannonritenour@gmail.com>
2021-04-05 17:14:07 -0500

    updated db query

--- src/server/routes/statues.ts
+++ src/server/routes/statues.ts
@@ -20,7 +20,6 @@ router.get('/:id?', async (req, res) => {
     }
 });
 
-
 router.post('/add', async (req, res) => {
     const newStatue = req.body;
     try {
@@ -32,18 +31,18 @@ router.post('/add', async (req, res) => {
     }
 });
 
-// router.put('/:id', async (req, res) => {
-//     const statueid = req.params.id;
-//     const editedStatue = req.body.content;    //switch out .content for whatever applies to statue
-//     try {
-//         await db.statues.put(statueid, editedStatue);
-//         res.json({ msg: 'single statue by id ' + statueid });
-//     }
-//     catch (error) {
-//         console.log(error);
-//         res.status(500).send(error);
-//     };
-// });
+router.put('/:id', async (req, res) => {
+    const statueid = req.params.id;
+    const editedStatue = req.body;    
+    try {
+        await db.statues.put(statueid, editedStatue);
+        res.json({ msg: 'single statue by id ' + statueid });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    };
+});
 
 router.delete('/:id', async (req, res) => {
     const statueid = req.params.id;
