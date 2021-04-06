8ac1d215ebb9b263a9f25c4eacab22b6c394e52a
Justin Kelley <justin.k.owen@gmail.com>
2021-04-06 17:06:03 -0500

    unfinished vulcandex

---
+++ src/client/components/flipcard.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import type { IStatue } from '../utils/types'
+import { Link } from 'react-router-dom'
+
+
+const FlipCard: React.FC<FlipCardProps> = ({ blog }) => {
+    return (
+       
+    )
+}
+
+interface FlipCardProps {
+    blog: IStatue
+}
+
+export default FlipCard
--- src/client/views/Vulcandex.tsx
+++ src/client/views/Vulcandex.tsx
@@ -1,14 +1,44 @@
 import * as React from 'react';
+import type { IStatue } from '../utils/types';
+import flipcard from '../components/flipcard'
+
+
+
 
 const Vulcandex: React.FC<VulcandexProps> = (props) => {
-    
+    const [statues, setStatues] = React.useState<IStatue[]>([]);
+
+    React.useEffect(() => {
+        (async () => {
+            const res = await fetch('/api/statues')
+            const statues = await res.json();
+            setStatues(statues);
+        })();
+
+    }, []);
+
+
     return (
         <main className="container">
-            
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <img src="./assets/BJCC.jpeg" className="BJCC"/>
+
+                    </div>
+
+                    <div className="flip-card-back">
+                        <h1>John Doe</h1>
+                        <p>Architect &amp; Engineer</p>
+                        <p>We love that guy</p>
+                    </div>
+
+                </div>
+            </div>
         </main>
     );
 };
 
-interface VulcandexProps {}
+interface VulcandexProps { }
 
 export default Vulcandex;
