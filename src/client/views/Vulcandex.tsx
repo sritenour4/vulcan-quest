46a3cf24dfac0536db506ec7ff0081731727e040
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-04-07 18:04:43 -0500

    working on media queries for mobile cards

--- src/client/views/Vulcandex.tsx
+++ src/client/views/Vulcandex.tsx
@@ -22,58 +22,117 @@ const Vulcandex: React.FC<VulcandexProps> = (props) => {
         <>
             {/* 1 */}
             <main className="row flex-container">
-                <div className="col-md-auto flip-card">
+                <div className="col-md-auto flip-card m-5 ">
                     <div className="flip-card-inner">
                         <div className="flip-card-front">
                             <img src="./assets/CardPreview.PNG" className="Preview" />
                         </div>
-                        <div className="flip-card-back">
+                        <div className="flip-card-back m-5 ">
                             <img src="./assets/BJCC.PNG" className="BJCC" />
                         </div>
                     </div>
                 </div>
                 {/* 2 */}
-                <div className="col-md-auto flip-card">
+                <div className="col-md-auto flip-card m-5 ">
                     <div className="flip-card-inner">
                         <div className="flip-card-front">
                             <img src="./assets/CardPreview.PNG" className="Preview" />
 
                         </div>
 
-                        <div className="flip-card-back">
+                        <div className="flip-card-back m-5 ">
                             <img src="./assets/BotanicalGardens.PNG" className="BotanicalGardens" />
                         </div>
 
                     </div>
                 </div>
                 {/* 3 */}
-                <div className="col-md-auto flip-card">
+                <div className="col-md-auto flip-card m-5 ">
                     <div className="flip-card-inner">
                         <div className="flip-card-front">
-                            <img src="./assets/CardPreview.PNG" className="Preview" />
-
+                            <img src="./assets/CardPreview.PNG" className="Preview"  />
                         </div>
 
-                        <div className="flip-card-back">
+                        <div className="flip-card-back m-5 ">
                             <img src="./assets/BhamZoo.PNG" className="BhamZoo" />
                         </div>
                     </div>
                 </div>
                 {/* 4 */}
-                <div className="col-md-auto flip-card">
+                <div className="col-md-auto flip-card m-5 ">
                     <div className="flip-card-inner">
                         <div className="flip-card-front">
                             <img src="./assets/CardPreview.PNG" className="Preview" />
 
                         </div>
 
-                        <div className="flip-card-back">
+                        <div className="flip-card-back m-5 ">
                             <img src="./assets/BhamSouthern.PNG" className="BhamSouthern" />
                         </div>
 
                     </div>
                 </div>
             </main>
+
+
+
+
+
+                        {/* 1 */}
+                        <main className="row flex-container">
+                <div className="col-md-auto flip-card m-5 ">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <img src="./assets/CardPreview.PNG" className="Preview" />
+                        </div>
+                        <div className="flip-card-back m-5 ">
+                            <img src="./assets/BJCC.PNG" className="BJCC" />
+                        </div>
+                    </div>
+                </div>
+                {/* 2 */}
+                <div className="col-md-auto flip-card m-5 ">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <img src="./assets/CardPreview.PNG" className="Preview" />
+
+                        </div>
+
+                        <div className="flip-card-back m-5 ">
+                            <img src="./assets/BotanicalGardens.PNG" className="BotanicalGardens" />
+                        </div>
+
+                    </div>
+                </div>
+                {/* 3 */}
+                <div className="col-md-auto flip-card m-5 ">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <img src="./assets/CardPreview.PNG" className="Preview"  />
+                        </div>
+
+                        <div className="flip-card-back m-5 ">
+                            <img src="./assets/BhamZoo.PNG" className="BhamZoo" />
+                        </div>
+                    </div>
+                </div>
+                {/* 4 */}
+                <div className="col-md-auto flip-card m-5 ">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <img src="./assets/CardPreview.PNG" className="Preview" />
+
+                        </div>
+
+                        <div className="flip-card-back m-5 ">
+                            <img src="./assets/BhamSouthern.PNG" className="BhamSouthern" />
+                        </div>
+
+                    </div>
+                </div>
+            </main>
+
+
 
         </>
     );
