3d70a0c77c23fbf84645ca9e58ca72e52cb0d68c
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-04-08 08:56:43 -0500

    added homepage cover image of the vulcan statue

--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -1,25 +1,25 @@
-import * as React from 'react';
-import { IStatue } from '../utils/types';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+// import coverImage from "../../../public/assets/home_vulcan_image.jpeg";
+// import tallImage from "../../../public/assets/tall_photo.jpeg";
+import { IStatue } from "../utils/types";
+import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 const Home: React.FC<HomeProps> = (props) => {
+  const [statues, setstatues] = React.useState<IStatue[]>([]);
 
-    const [statues, setstatues] = React.useState<IStatue[]>([]);
+  React.useEffect(() => {
+    (async () => {
+      const res = await fetch("/api/statues");
+      const statues = await res.json();
+      setstatues(statues);
+    })();
+  }, []);
 
-    React.useEffect(() => {
-        (async () => {
-            const res = await fetch('/api/statues');
-            const statues = await res.json();
-            setstatues(statues);
-        })();
-    }, []);
-
-    return (
-
-        <div className="d-flex flex-row m-3">
-            <main className="row justify-content-center">
-
-                {statues.map(statue => (
+  return (
+    <div className="d-flex flex-row m-3">
+      <main className="row justify-content-center">
+        {/* {statues.map(statue => (
                     <div className="col-md-3 mx-1 mb-3 float-left" key={`statue-card-${statue.id}`}>
                         <div className="card shadow">
                             <img className="card-img-top img-fluid" src="..." alt="Card image cap"/>
@@ -31,14 +31,32 @@ const Home: React.FC<HomeProps> = (props) => {
                             </div>
                         </div>
                     </div>
-                ))}
-               
-            </main>
-        </div>
+                ))} */}
 
-    );
+        <div>
+          <div>
+            <h1>Cover Img</h1>
+          </div>
+          <img
+            className="smallImgMedia full-bg-img"
+            id="picblock"
+            src="/assets/home_vulcan_image.jpeg"
+            alt="Vulcan Statue Image"
+          />
+          {/* <img className="tallImgMedia" src={tallImage} /> */}
+          {/* <div className="boxMainImage">
+                    </div> */}
+
+          {/* <div className="App"> */}
+          {/* <div className="box">
+                        </div> */}
+          {/* </div> */}
+        </div>
+      </main>
+    </div>
+  );
 };
 
-interface HomeProps { }
+interface HomeProps {}
 
 export default Home;
