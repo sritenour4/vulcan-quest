360636d8c509c47203608678941119779ebf2d1e
Shannon Ritenour <shannonritenour@gmail.com>
2021-04-09 15:13:53 -0500

    added bootstrap styling to cards

--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -16,21 +16,21 @@ const Home: React.FC<HomeProps> = (props) => {
 
     return (
         <>
-            <header className="row">
-                <img className="logo" src="/assets/vulcan-quest-logo.PNG" alt="vulcan-quest-logo" />
+            <header className="row d-flex justify-content-center align-items-center">               
+                <img className="logo" src="/assets/vulcan-quest-logo.PNG" alt="vulcan-quest-logo" />               
             </header>
 
-            <div className="d-flex flex-row m-3">
+            <div className="d-flex flex-fill m-2">
                 <main className="row justify-content-center">
 
                     {statues.map(statue => (
-                        <div className="col-md-3 mx-1 mb-3 float-left" key={`statue-card-${statue.id}`}>
-                            <div className="card shadow">
+                        <div className="col-md-3 d-flex p-1 mx-1 mb-3" key={`statue-card-${statue.id}`}>
+                            <div className="card shadow bg-danger">
                                 <img className="card-img-top img-fluid" src={`/assets/vulcan-${statue.id}.PNG`} alt="vulcan"></img>
                                 <div className="card-body">
-                                    <h5 className="card-title text-center">{statue.title}</h5>
-                                    <div className="card-link btn btn-sm btn-outline-primary float-right">
-                                        <Link to={`/details/${statue.id}`}>Read More!</Link>
+                                    <h5 className="card-title text-center text-warning font-weight-bold">{statue.title}</h5>
+                                    <div className="card-link text-primary btn btn-sm btn-info float-right">
+                                        <Link to={`/details/${statue.id}`}>Read More</Link>
                                     </div>
                                 </div>
                             </div>
