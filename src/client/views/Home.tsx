808709941b9e0fd73a0b1e4975fc45f68d3371ae
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-04-11 16:21:50 -0500

    added class name to homepage cards and brought the homepage cards to the front using z-index

--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -1,43 +1,51 @@
-import * as React from 'react';
-import { IStatue } from '../utils/types';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { IStatue } from "../utils/types";
+import { Link } from "react-router-dom";
 
 const Home: React.FC<HomeProps> = (props) => {
+  const [statues, setstatues] = React.useState<IStatue[]>([]);
 
-    const [statues, setstatues] = React.useState<IStatue[]>([]);
+  React.useEffect(() => {
+    (async () => {
+      const res = await fetch("/api/statues");
+      const statues = await res.json();
+      setstatues(statues);
+    })();
+  }, []);
 
-    React.useEffect(() => {
-        (async () => {
-            const res = await fetch('/api/statues');
-            const statues = await res.json();
-            setstatues(statues);
-        })();
-    }, []);
+  return (
+    <>
+      <header className="row d-flex justify-content-center align-items-center">
+        <img
+          className="logo"
+          src="/assets/vulcan-quest-logo.PNG"
+          alt="vulcan-quest-logo"
+        />
+      </header>
 
-    return (
-        <>
-            <header className="row d-flex justify-content-center align-items-center">               
-                <img className="logo" src="/assets/vulcan-quest-logo.PNG" alt="vulcan-quest-logo" />               
-            </header>
-
-            <div className="d-flex flex-fill m-2">
-                <main className="row justify-content-center">
-
-                    {statues.map(statue => (
-                        <div className="col-md-3 d-flex p-1 mx-1 mb-3" key={`statue-card-${statue.id}`}>
-                            <div className="card shadow bg-danger">
-                                <img className="card-img-top img-fluid" src={`/assets/vulcan-${statue.id}.PNG`} alt="vulcan"></img>
-                                <div className="card-body">
-                                    <h5 className="card-title text-center text-warning font-weight-bold">{statue.title}</h5>
-                                    <div className="card-link text-primary btn btn-sm btn-info float-right">
-                                        <Link to={`/details/${statue.id}`}>Read More</Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-            //         ))}
-            //     </main>
-            // </div>
+      <div className="d-flex flex-fill m-2">
+        <main className="row justify-content-center">
+          {statues.map((statue) => (
+            <div
+              className="col-md-3 d-flex p-1 mx-1 mb-3 homepageCards"
+              key={`statue-card-${statue.id}`}
+            >
+              <div className="card shadow bg-danger">
+                <img
+                  className="card-img-top img-fluid"
+                  src={`/assets/vulcan-${statue.id}.PNG`}
+                  alt="vulcan"
+                ></img>
+                <div className="card-body">
+                  <h5 className="card-title text-center text-warning font-weight-bold">
+                    {statue.title}
+                  </h5>
+                  <div className="card-link text-primary btn btn-sm btn-info float-right">
+                    <Link to={`/details/${statue.id}`}>Read More</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
           ))}
 
           <div className="coverImgContainer">
@@ -53,6 +61,6 @@ const Home: React.FC<HomeProps> = (props) => {
   );
 };
 
-interface HomeProps { }
+interface HomeProps {}
 
 export default Home;
