6e60d94b7498c0136c8e98d205aaf6b8cad5afe6
H-Thomas4 <Heatherthomas03@gmail.com>
2021-04-10 19:37:49 -0500

    changed color links

--- src/client/components/NavBar.tsx
+++ src/client/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const NavBar: React.FC<NavBarProps> = (props) => {
   return (
-    <nav className="nav justify-content-center shadow p-3 mb-2">
+    <nav className="nav justify-content-center shadow p-3 mb-3">
       <NavLink
         exact
         className="font-weight-light mx-5"
