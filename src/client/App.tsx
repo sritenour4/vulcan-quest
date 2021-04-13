cb2f56650e39d2a09f9db088b5bf99e38c94e810
Shannon Ritenour <shannonritenour@gmail.com>
2021-04-13 09:56:23 -0500

    added signup and login routes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,7 +4,8 @@ import NavBar from './components/NavBar';
 import Home from './views/Home';
 import Details from './views/Details';
 import Vulcandex from './views/Vulcandex';
-
+import Login from './views/Login';
+import Signup from './views/Signup';
 
 
 const App: React.FC<AppProps> = (props) => {
@@ -12,21 +13,27 @@ const App: React.FC<AppProps> = (props) => {
 		<BrowserRouter>
 			<NavBar />
 			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/details/:statueid">
-					<Details />
-				</Route>
-				<Route exact path="/vulcandex/">
-					<Vulcandex />
-				</Route>				
+				<Route exact path="/signup">
+					<Signup />
+					</Route>
+					<Route exact path="/login">
+						<Login />
+					</Route>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/details/:statueid">
+						<Details />
+					</Route>
+					<Route exact path="/vulcandex/">
+						<Vulcandex />
+					</Route>
 			</Switch>
 		</BrowserRouter>
 	);
 };
 
-interface AppProps { };
+interface AppProps {};
 
 export default App;
 
