1f44305687f459249cb2f3b90530b93dcef91fe1
Shannon Ritenour <shannonritenour@gmail.com>
2021-04-13 10:42:27 -0500

    commented out login/signup until code is added

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,8 +4,8 @@ import NavBar from './components/NavBar';
 import Home from './views/Home';
 import Details from './views/Details';
 import Vulcandex from './views/Vulcandex';
-import Login from './views/Login';
-import Signup from './views/Signup';
+// import Login from './views/Login';
+// import Signup from './views/Signup';
 
 
 const App: React.FC<AppProps> = (props) => {
@@ -13,12 +13,12 @@ const App: React.FC<AppProps> = (props) => {
 		<BrowserRouter>
 			<NavBar />
 			<Switch>
-				<Route exact path="/signup">
+				{/* <Route exact path="/signup">
 					<Signup />
 				</Route>
 				<Route exact path="/login">
 					<Login />
-				</Route>
+				</Route> */}
 				<Route exact path="/">
 					<Home />
 				</Route>
